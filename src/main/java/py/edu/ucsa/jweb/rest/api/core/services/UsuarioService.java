6f5eae3e55b92ed16f4adb63951a6df58ceac5bb
hugoever <hugoever@gmail.com>
2024-07-03 15:48:37 -0400

    Actualizacion de Servicios jpa

--- src/main/java/py/edu/ucsa/jweb/rest/api/core/services/UsuarioService.java
+++ src/main/java/py/edu/ucsa/jweb/rest/api/core/services/UsuarioService.java
@@ -2,17 +2,18 @@ package py.edu.ucsa.jweb.rest.api.core.services;
 
 import java.util.List;
 
+import py.edu.ucsa.jweb.rest.api.core.entities.Usuario;
 import py.edu.ucsa.jweb.rest.api.web.dto.UsuarioDTO;
 
-public interface UsuarioService {
+public interface UsuarioService extends GenericService<Integer, Usuario>{
 	
 	List<UsuarioDTO> listarTodos();
 	UsuarioDTO getById(long Id);
-	UsuarioDTO getByUsuario(String usuario);
+	Usuario getByUsuario(String usuario);
 	void crearUsuario(UsuarioDTO usuario);
 	void actualizarUsuario(UsuarioDTO usuario);
 	void eliminarUsuario(long id);
 	void eliminarTodos();
-	boolean isExisteUsuario(UsuarioDTO usuario);
+	boolean isExisteUsuario(Usuario usuario);
 
 }
